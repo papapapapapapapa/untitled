402ac24e86c4558097c0adfc8cc6cf318682f9e9
papapapapapapapa <r3386588@gmail.com>
2017-10-15 01:32:28 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        Scanner in=new Scanner(System.in);
+        float x;
+        x=in.nextFloat();
+        System.out.printf("%.2f",x*1.6);
+        System.out.println();
+    }
+}
